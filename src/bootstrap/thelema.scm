13c7ae379fc63d99e22cfae07fa697176139ffb6
Jay Osako <JosephOsako@gmail.com>
2014-04-04 07:45:59 -0400

    Added src/ and tests/ directories, and a starting set of files for the bootstrap (R6RS Scheme) version.

---
+++ src/bootstrap/thelema.scm
@@ -0,0 +1,4 @@
+#lang racket
+
+(require (planet schematics/schemeunit:3:5))
+
---
+++ tests/readtest.scm
@@ -0,0 +1,6 @@
+#lang racket
+
+(require (planet schematics/schemeunit:3:5))
+
+(define-test-suite tokenizer-tests
+  (test-case 
